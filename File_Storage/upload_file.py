d2ca533d00060e4ff81ec6f36c9ac1f18da6e69c
JJLI <jerryleevip@163.com>
2017-03-07 17:54:32 +0800

    sync to github

--- File_Storage/upload_file.py
+++ File_Storage/upload_file.py
@@ -1,5 +1,13 @@
 from azure.storage.file import FileService
+from azure.storage.file import ContentSettings
 
 file_service = FileService(account_name='mybiccstorage', account_key='VdxZ3Gd6UN+I1iIWH78NSMehp72GUSJFkb6kSEyo2PXX98rfW+ePlMaPdaYcs+4MH8mRl+EaDT+6GcpIorwKgw==')
 
-file_service.create_share('testsharejerry')
+# file_service.create_share('testsharejerry')
+
+file_service.create_file_from_path(
+    'testsharejerry',
+    None, # We want to create this blob in the root directory, so we specify None for the directory_name
+    '20170301135604.png',
+    '20170301135604.png',
+    content_settings=ContentSettings(content_type='image/png'))
